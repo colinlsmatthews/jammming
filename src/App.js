743ef1d685e98779ef9c22fa8759791f17d5b3a5
Colin Matthews <25379751+colinlsmatthews@users.noreply.github.com>
2024-08-29 07:29:48 -0400

    implement search bar animation

--- src/App.js
+++ src/App.js
@@ -10,22 +10,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <SearchBar />
+        <h1>Spotify Playlist App</h1>
+        <SearchBar Label="Some Label"/>
         <SearchResults />
         <Tracklist />
-        <Button />
+        <Button Content="Test Button"/>
       </header>
     </div>
   );
--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./SearchBar.css"
 
-function SearchBar() {
+
+function SearchBar(props) {
+  const { Label = "", 
+          Placeholder = "Search...", 
+          ...restProps } = props;
+  
+  useEffect(() => {
+    const input = document.getElementById("search-input");
+    const searchBtn = document.getElementById("search-btn");
+    const label = document.getElementById("search-label");
+    
+
+    const expand = () => {
+      searchBtn.classList.toggle("close");
+      input.classList.toggle("square");
+      label.classList.toggle("label-on");
+    };
+    
+    if (searchBtn) {
+      searchBtn.addEventListener("click", expand);
+    }
+
+    // Cleanup
+    return () => {
+      if (searchBtn) {
+        searchBtn.removeEventListener("click", expand);
+      }
+    };
+  }, []);
+
+
   return (
-    <div className="search-bar">
-      <label htmlFor="search">Search:</label>
-      <input type="text" placeholder="Search..." />
-    </div>
+    <form id="content">
+      <input type="text" name="input" class="input" id="search-input" ></input>
+      <button type="reset" class="search" id="search-btn"></button>
+      <label html-for="input" class="label-off" id="search-label">Test</label>
+    </form>
   );
 }
 
